da31b9fdcf4603238d6d4ed4275e55f2472e4ab2
zeman01 <deshadow95@gmail.com>
2025-10-19 08:34:07 +0545

    express Server and mangoDB

--- express_server/src/controllers/users.controllers.js
+++ express_server/src/controllers/users.controllers.js
@@ -1,114 +1,141 @@
 const users = [];
 
+// add mongo db schema and model here in future
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
+// model
+const User = mongoose.model("user", userSchema);
 //get all users
 export const getAll = async (req, res) => {
-    try {
-        // Simulate async operation
-        res.status(200).json({
-            data: users,
-            message: 'Users fetched successfully'
-        });
-    } catch (error) {
+  try {
+    // Simulate async operation
+    const users = await User.find({});
+    res.status(200).json({
+      data: users,
+      message: "Users fetched successfully",
+    });
+  } catch (error) {
     res.status(500).json({
-         message: 'Internal Server Error' });
-}}; 
+      message: "Internal Server Error",
+    });
+  }
+};
 
 // get user by id
 export const getById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = users.find(user => user.id.toString() === id);
-        if (!user) {
-            res.status(404).json({
-                message: 'User not found',
-                data: null
-            });
-            return;
-        }
-        res.status(200).json({
-            data: user,
-            message: 'User fetched successfully'
-        });
-
-    } catch (error) {
+  try {
+    const { id } = req.params;
+    // const user = users.find(user => user.id.toString() === id);
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({
+        message: "User not found",
+        data: null,
+      });
+      return;
+    }
+    res.status(200).json({
+      data: user,
+      message: "User fetched successfully",
+    });
+  } catch (error) {
     res.status(500).json({
-         message: 'Internal Server Error' });
-}};
+      message: "Internal Server Error",
+    });
+  }
+};
 
 // create user
-export const createUser  = async (req, res) => {
-    try {
-        const data= req.body;
-if(!data){
-    res.status(400).json({
-        message: 'data expected',
-        data: null
-    });
-    return;
-}else{
-    users.push({...data, id: users.length + 1});
+export const createUser = async (req, res) => {
+  try {
+    const data = req.body;
+    console.log(req.body);
+    if (!data) {
+      res.status(400).json({
+        message: "data expected",
+        data: null,
+      });
+      return;
+    }
+    // users.push({ ...data, id: users.length + 1 });
+    //  write in database
+
+    const user = await User.create(data);
+  
     res.status(201).json({
-        
-        message: 'New User created ',
-        data: users.at(-1)
+      message: "New User created ",
+      data: users.at(-1),
     });
-}
-    } catch (error) {
+  } catch (error) {
     res.status(500).json({
-         message: 'Internal Server Error' });
-}};
+      message: "Internal Server Error",
+    });
+  }
+};
 
 // Update User
 export const updateUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const data= req.body;
-if(!data){
-    res.status(400).json({
-        message: 'data expected',
-        data: null
-    });
-    return;
-}else{
-    const userIndex = users.findIndex(user => user.id.toString() === id);
-    if (userIndex === -1) {
-        res.status(404).json({
-            message: 'User not found',
-            data: null
-        });
-        return;
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    if (!data) {
+      res.status(400).json({
+        message: "data expected",
+        data: null,
+      });
+      return;
     }
-    users[userIndex] = { ...users[userIndex], ...data };
-    res.status(200).json({
-        
-        message: 'User updated successfully',
-        data: users[userIndex]
-    });
-}
-    } catch (error) {
+    //   const userIndex = users.findIndex((user) => user.id.toString() === id);
+
+    const user = await User.findByIdAndUpdate(id, data,{ new: true });
+          
+      res.status(201).json({
+        message: "User updated successfully",
+        data: users[userIndex],
+      });
+    
+  } catch (error) {
     res.status(500).json({
-         message: 'Internal Server Error' });
-}}; 
+      message: "Internal Server Error",
+    });
+  }
+};
 
 // Delete User
 export const deleteUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const userIndex = users.findIndex(user => user.id.toString() === id);
-        if (userIndex === -1) {
-            res.status(404).json({
-                message: 'User not found',
-                data: null
-            });
-            return;
-        }
-        const deletedUser = users.splice(userIndex, 1);
-        res.status(200).json({
-            message: 'User deleted successfully',
-            data: deletedUser[0]
-        });
-    } catch (error) {
+  try {
+    const { id } = req.params;
+    // const userIndex = users.findIndex((user) => user.id.toString() === id);
+    await User.findByIdAndDelete(id);
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({
+        message: "User not found",
+        data: null,
+      });
+      return;
+    }
+
+    res.status(200).json({
+      message: "User deleted successfully",
+      data: deletedUser[0],
+    });
+  } catch (error) {
     res.status(500).json({
-         message: 'Internal Server Error' });
-}};
+      message: "Internal Server Error",
+    });
+  }
+};
--- express_server/src/server.js
+++ express_server/src/server.js
@@ -1,4 +1,5 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 
 //* server instance
 const app = express();
@@ -7,17 +8,24 @@ const app = express();
 const PORT = 8080;
 
 //* importing routes
-import userRoutes from './routes/users.route.js';
+import userRoutes from "./routes/users.route.js";
+
+// * mongoose connection
+mongoose
+  .connect("mongodb:// localhost:27017/new_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 //* using routes
-app.use('/api/users', userRoutes);
-
+app.use("/api/users", userRoutes);
 
 // *using express json middlewares
 app.use(express.json());
 
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
